7436a5c43da1bb75e0b75d7984b6a4c02348b489
Anand <72251918+HellBoy-OP@users.noreply.github.com>
2023-11-28 00:27:30 +0530

    resolved massive conflicts

--- Medicines.vb
+++ Medicines.vb
@@ -1,9 +1,3 @@
 ﻿Public Class Medicines
-    Private Sub Guna2CustomGradientPanel1_Paint(sender As Object, e As PaintEventArgs) Handles Guna2CustomGradientPanel1.Paint
 
-    End Sub
-
-    Private Sub Label5_Click(sender As Object, e As EventArgs) Handles Label5.Click
-
-    End Sub
 End Class
