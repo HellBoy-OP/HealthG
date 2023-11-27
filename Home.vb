0a4fb77bf787673f455283d762e560afaa0dee50
Anand <72251918+HellBoy-OP@users.noreply.github.com>
2023-11-28 02:59:53 +0530

    hii

--- Home.vb
+++ Home.vb
@@ -1,9 +1,3 @@
 ﻿Public Class Home
-    Private Sub Guna2Separator1_Click(sender As Object, e As EventArgs) Handles Guna2Separator1.Click
 
-    End Sub
-
-    Private Sub Guna2HtmlLabel4_Click(sender As Object, e As EventArgs) Handles Guna2HtmlLabel4.Click
-
-    End Sub
 End Class
